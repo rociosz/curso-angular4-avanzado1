db77a5b678fb9546e42206c0b7d2e952cf5a78e2
rociosz <rocio.sz.29@gmail.com>
2019-07-30 14:45:51 -0500

    Sección 12 - Animaciones Simples con Estados en Angular

--- src/app/components/tienda/tienda.component.ts
+++ src/app/components/tienda/tienda.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 declare var jQuery:any;
 declare var $:any;
@@ -6,26 +7,42 @@ declare var $:any;
 @Component({
   selector: 'tienda',
   templateUrl: './tienda.component.html',
-  styleUrls: ['./tienda.component.css']
+  styleUrls: ['./tienda.component.css'],
+  animations: [
+    trigger('marcar', [
+        state('inactive', style({
+          border: '3px solid #ccc'
+        })),
+        state('active', style({
+          border: '3px solid yellow',
+          background: 'red',
+          borderRadius: '50px',
+          transform: 'scale(1.2)'
+        })),
+        transition('inactive => active', animate('300ms linear')),
+        transition('active => inactive', animate('300ms linear'))
+    ])
+  ]
 })
 export class TiendaComponent implements OnInit {
   public titulo;
   public nombreDelParque: string;
   public miParque;
+  public status;
 
   constructor() {
     this.titulo = 'Esta es la Tienda de Uniformes Escolares';
+    this.status = 'inactive'
   }
 
-  mostrarNombre(){
-    console.log(this.nombreDelParque);
+  cambiarEstado(status){
+    if(status == 'inactive'){
+        this.status = 'active';
+    }else{
+        this.status = 'inactive';
+    }
   }
-
-  verDatosParque(event){
-    console.log(event);
-    this.miParque = event;
-  }
-
+  
   ngOnInit() {
     $('#textojq').hide();
      $('#botonjq').click(function(){
