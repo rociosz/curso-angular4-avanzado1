5c78954f2116359e9baf679bede088484a1cbbd0
rociosz <rocio.sz.29@gmail.com>
2019-07-31 17:42:08 -0500

    Sección 14 - Crear Modelos

---
+++ src/app/models/alumno.ts
@@ -0,0 +1,9 @@
+export class Alumno{
+    constructor(
+        public name: string,
+        public description: string,
+        public year: number,
+        public image: string,
+        public user: string
+    ){}
+}
