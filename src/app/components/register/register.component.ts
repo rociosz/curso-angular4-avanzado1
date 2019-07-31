7530c075c42080a0bfbb53d99e6bcfe7eef37c90
rociosz <rocio.sz.29@gmail.com>
2019-07-31 17:51:18 -0500

    Sección 14 - Recibir los Datos del Formulario de Registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
 
 @Component({
     selector: 'register',
@@ -7,6 +8,7 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
   })
   export class RegisterComponent implements OnInit {
     public title: string;
+    public user: User;
   
     constructor(
         private _route: ActivatedRoute,
@@ -14,9 +16,15 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
     ){
         this.title = 'Registro';
+        this.user = new User('','','','','','ROLE_USER','');
     }
 
     ngOnInit(){
         console.log('register.component cargado !!');
     }
+
+    onSubmit(){
+      console.log(this.user);
+    }
+
   }
