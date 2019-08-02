fb1eae4ec7424b3b85d7fc5aa5d070feef82aac9
rociosz <rocio.sz.29@gmail.com>
2019-08-01 21:18:17 -0500

    Sección 15 - Vincular un objeto de Usuario al Formulario de Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,23 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/user';
+
 @Component({
     selector: 'login',
     templateUrl: './login.component.html',
   })
   export class LoginComponent implements OnInit {
     public title: string;
+    public user: User;
   
     constructor(
         private _route: ActivatedRoute,
         private _router: Router,
     ){
        this.title = 'Login de Usuarios';
+       this.user = new User('','','','','','ROLE_USER','');
     }
 
     ngOnInit(){
         console.log('login.component cargando !!');
     }
     
+    onSubmit(){
+        console.log(this.user);
+    }
 }
   
